ac596247289a0c1bd881c17da68fbe9c88023cfb
Shiul93 <lfllamas93@gmail.com>
2016-11-24 13:08:44 +0100

    Added license

--- remotelib.js
+++ remotelib.js
@@ -1,4 +1,24 @@
 
+/*******************************************************************************
+ * Copyright 2016 Mytech Ingenieria Aplicada <http://www.mytechia.com>
+ * Copyright 2016 Luis Llamas <luis.llamas@mytechia.com>
+ * <p>
+ * This file is part of Robobo Scratch Extension.
+ * <p>
+ * Robobo Scratch Extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * Robobo Scratch Extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Robobo Scratch Extension.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 //Constructor of the remote control object
 function Remote(ip){
   this.ip = ip;
